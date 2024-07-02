e599067897c6455ce324eaf9da5723dc7a05c6ca
Mohit <me.mohitsharma14@gmail.com>
2024-07-02 22:32:07 +0530

    Part 3 almost done

---
+++ Part 3/script.js
@@ -0,0 +1,13 @@
+var initialPath =`M 10 100 Q 250 100 490 100`
+
+var finalPath =`10 100 Q 250 100 490`
+var string= document.querySelector("#string")
+
+string.addEventListener("mouseenter",function(){
+    console.log("entered")
+})
+
+
+string.addEventListener("mouseleave",function(){
+    console.log("leave")
+})
